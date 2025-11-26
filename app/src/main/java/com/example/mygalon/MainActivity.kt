b444b54a30336ed75c1a548bd9777976b7560e19
SayonaraMoruten <reeimu28@gmail.com>
2025-11-26 19:34:37 +0700

    nambahin LoginActivity.kt, package dan fragment buat home nanti.

--- app/src/main/java/com/example/mygalon/MainActivity.kt
+++ app/src/main/java/com/example/mygalon/MainActivity.kt
@@ -5,6 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val email: String = intent.getStringExtra(LoginActivity.KEY_USERNAME) ?:""
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewGalon) //bikin recyclerview galon
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val listGalon = List<galon> = getData() // ngambil data dari list galon
+        recyclerView.adapter = GalonAdapter(listGalon, onClickItemGalon::goToRincianGalon) //belum bikin class rincian galon
     }
+}
+
+fun getData(): List<galon>{
+    val listGalon = muteableListOf<galon>()
+// data galon nanti (nama,harga, merk (enum class))
 }
